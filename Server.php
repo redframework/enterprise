59de82d4f66791c99566c1c51a27fc2c3a1b9601
Mahbod Azizkhani <54548586+redcoder927@users.noreply.github.com>
2020-01-10 04:14:23 +0330

    Update Server.php

--- Server.php
+++ Server.php
@@ -9,10 +9,15 @@ define('php_development_server', TRUE);
 
 $_SERVER['SERVER_ADDR'] = "127.0.0.1";
 
-if (strpos($_SERVER['REQUEST_URI'], '.') == TRUE) {
-    require_once urldecode(trim($_SERVER['REQUEST_URI'], "\/"));
+if (!isset($_GET['url']) && strpos($_SERVER['REQUEST_URI'], '.') == TRUE) {
+    $path = urldecode(trim($_SERVER['REQUEST_URI'], "\/"));
+    if (file_exists($path)) {
+        require_once $path;
+    } else {
+        header("Location: /?url=" . trim($_SERVER['REQUEST_URI'], "\/"));
+    }
 } else if (!isset($_GET['url'])) {
-    if (trim($_SERVER['REQUEST_URI'], "\/")){
+    if (trim($_SERVER['REQUEST_URI'], "\/")) {
         header("Location: /?url=" . trim($_SERVER['REQUEST_URI'], "\/"));
     } else {
         require_once 'public/index.php';
